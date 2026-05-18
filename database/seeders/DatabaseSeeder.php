17e9c8578c886f0738500c06a15b32674df064f8
mikawzuki-cell <augusmika4@gmail.com>
2026-05-18 22:42:55 +0800

    Connection to the databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Call other seeders
+        $this->call([
+            ChurchManagementSeeder::class,
+        ]);
 
+        // Sample user
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
